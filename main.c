76b13ee1eed7bde5e3194050906d917f16d410aa
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-08-19 16:40:29 +0900

    bind socket.

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <netinet/in.h>
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-  int fd;
+  struct sockaddr_in addr;
+  int sv_sock;
 
-  fd = socket(AF_INET, SOCK_STREAM, 0);
-  close(fd);
+  /* create a socket, endpoint of connection */
+  sv_sock = socket(AF_INET, SOCK_STREAM, 0);
 
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(12345);
+  addr.sin_addr.s_addr = INADDR_ANY;
+  bind(sv_sock, (struct sockaddr *)&addr, sizeof(addr));
+  
+  close(sv_sock);
+  
   return 0;
 }
