1145a3e6607a280649fbf4c238fceda89411f548
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-08-19 16:55:23 +0900

    introduce hello

--- main.c
+++ main.c
@@ -1,21 +1,30 @@
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netinet/in.h>
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-  struct sockaddr_in addr;
   int sv_sock;
 
   /* create a socket, endpoint of connection */
   sv_sock = socket(AF_INET, SOCK_STREAM, 0);
 
+  struct sockaddr_in addr;
   addr.sin_family = AF_INET;
   addr.sin_port = htons(12345);
   addr.sin_addr.s_addr = INADDR_ANY;
   bind(sv_sock, (struct sockaddr *)&addr, sizeof(addr));
-  
+
+  listen(sv_sock, 5);
+
+  struct sockaddr_in client;
+  socklen_t len = sizeof(client);
+  int sock = accept(sv_sock, (struct sockaddr *)&client, &len);
+
+  write(sock, "Hello", 5);
+
+  close(sock);
   close(sv_sock);
-  
+
   return 0;
 }
